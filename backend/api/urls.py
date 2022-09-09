c989fd7ce4e8d52936b407dceda878115a72f1fa
roddhenrick369 <roddhenrick369@gmail.com>
2022-09-09 15:32:18 -0300

    fix endpoint

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import ListMenu, DetailMenu
 
 urlpatterns = [
-    path('<int:pk>/', DetailMenu.as_view()),
+    path('menu/<int:pk>/', DetailMenu.as_view()),
     path('menu', ListMenu.as_view()),
 ]
